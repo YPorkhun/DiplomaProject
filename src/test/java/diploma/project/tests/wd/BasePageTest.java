fd07a54091a032cd3ca2c6bebd2c486304a3cc88
Yuliia Porkhun <julposv@gmail.com>
2023-07-25 18:13:07 +0300

    Registration test added

--- src/test/java/diploma/project/tests/wd/BasePageTest.java
+++ src/test/java/diploma/project/tests/wd/BasePageTest.java
@@ -1,32 +1,33 @@
 package diploma.project.tests.wd;
 import diploma.project.tests.listeners.CustomExtentReportsListener;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
-@Listeners({CustomExtentReportsListener.class})
 
 public class BasePageTest {
     protected WebDriver driver;
+
+    @BeforeClass
+    @Parameters({"browser"})
+    public void setUpBrowser(String browser) {
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+        } else if (browser.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+        } else throw new Error("You should set up the browser");
+    }
 
     @BeforeMethod
     @Parameters({"browser"})
     public void creatingObjectOfBrowser(String browser) {
         if (browser.equals("chrome")) {
             driver = new ChromeDriver();
-            ChromeOptions options = new ChromeOptions();
-            Map<String, Object> prefs = new HashMap<String, Object>();
-            prefs.put("download.default_directory", new File("target/downloads").getAbsolutePath());
-            options.setExperimentalOption("prefs", prefs);
         } else if (browser.equals("firefox")) {
             driver = new FirefoxDriver();
         } else throw new Error("You should creating an object of browser class");
-
         driver.manage().window().maximize();
     }
 
--- src/test/java/diploma/project/tests/wd/RegistrationPageTest.java
+++ src/test/java/diploma/project/tests/wd/RegistrationPageTest.java
@@ -1,4 +1,19 @@
 package diploma.project.tests.wd;
+import diploma.project.wd.RegistrationPage;
+import org.testng.annotations.Test;
 
 public class RegistrationPageTest extends BasePageTest {
+    @Test
+    public void RegistrationTest(){
+        driver.get("https://freelance.lsrv.in.ua/register");
+
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+        registrationPage.setUserName("yuliiap");
+        registrationPage.setName("Yuliia");
+        registrationPage.setLastname("Porkhub");
+        registrationPage.setPasswordAndConfirm("yuliiap");
+        registrationPage.clickOnRegisterButton();
+
+
+    }
 }
